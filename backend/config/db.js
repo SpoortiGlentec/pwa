9b8cfd396925e55ed04fa6536a1e05a51b811346
SpoortiGlentec <Spoorti.Angadi@glentec-digital.com>
2026-07-01 22:29:20 +0530

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,37 @@
+// const mysql = require("mysql2");
+// require("dotenv").config();
+
+// const connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// });
+
+// connection.connect((err) => {
+//     if (err) {
+//         console.log("Database Connection Failed");
+//         console.log(err);
+//         return;
+//     }
+
+//     console.log("MySQL Connected Successfully");
+// });
+
+// module.exports = connection;
+
 const mysql = require("mysql2");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 connection.connect((err) => {
--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,10 +1,23 @@
+// import axios from "axios";
+
+// // const BASE_URL = "http://localhost:5000/users";
+// const BASE_URL = "http://192.168.31.123:5000/users";
+// export const saveUser = (user) => {
+//     return axios.post(BASE_URL, user);
+// };
+// export const getUsers = () => {
+//     return axios.get(BASE_URL);
+// };
+
+
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:5000/users";
-const BASE_URL = "http://192.168.31.123:5000/users";
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 export const saveUser = (user) => {
-    return axios.post(BASE_URL, user);
+  return axios.post(BASE_URL, user);
 };
+
 export const getUsers = () => {
-    return axios.get(BASE_URL);
+  return axios.get(BASE_URL);
 };
