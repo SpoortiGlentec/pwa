7df271c227a61a6f171749fab9b1e4018df887fd
SpoortiGlentec <Spoorti.Angadi@glentec-digital.com>
2026-07-03 09:46:21 +0530

    added the changed psswd

--- backend/config/db.js
+++ backend/config/db.js
@@ -21,27 +21,29 @@
 // module.exports = connection;
 
 const mysql = require("mysql2");
+const fs = require("fs");
+const path = require("path");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {
-        rejectUnauthorized: true
-    }
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, "../certs/isrgrootx1.pem"))
+  }
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.log("Database Connection Failed");
-        console.log(err);
-        return;
-    }
+  if (err) {
+    console.error("Database Connection Failed");
+    console.error(err);
+    return;
+  }
 
-    console.log("MySQL Connected Successfully");
+  console.log("MySQL Connected Successfully");
 });
 
 module.exports = connection;
